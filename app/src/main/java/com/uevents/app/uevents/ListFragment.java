1b8f9e65193410ac0e681b0587450925fab6d62d
Daniel Cha <dcha27@gmail.com>
2016-11-29 19:38:05 -0500

    Tab Fragments

---
+++ app/src/main/java/com/uevents/app/uevents/ListFragment.java
@@ -0,0 +1,18 @@
+package com.uevents.app.uevents;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Daniel on 11/29/16.
+ */
+
+public class ListFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.list_fragment, container, false);
+    }
+}
