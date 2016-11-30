67e3175f9f85353c539e811dd58b1cc8f5a23e12
Rebecca He <acceber95@gmail.com>
2016-11-30 14:34:59 -0500

    Update color palette; Add Event class

---
+++ app/src/main/java/com/uevents/app/uevents/Event.java
@@ -0,0 +1,54 @@
+package com.uevents.app.uevents;
+
+import java.util.Date;
+
+/**
+ * Created by rebeccahe on 11/30/16.
+ */
+public class Event {
+
+    // fields
+    EventType eventType;
+    String title;
+    String description;
+    String location; // TODO: How are Google Map API locations stored?
+
+    Date startTime;
+    Date endTime;
+
+    int maxAttendance;
+    int currAttendance;
+
+
+    public enum EventType {
+        SPORT, STUDY, SOCIAL, CLUB
+    }
+
+    public Event(EventType eventType, String title, String description, String location,
+                 Date startTime, Date endTime, int maxAttendance) {
+        this.eventType = eventType;
+        this.title = title;
+        this.description = description;
+        this.location = location;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.maxAttendance = maxAttendance;
+        this.currAttendance = 0; // TODO: Should the default attendance be 1? Do we assume the creator is attending?
+    }
+
+    /**
+     * This method increments the attendance count if possible. If increment is successful, method
+     * returns true, otherwise returns false.
+     * @return
+     */
+    public boolean incrementAttendance() {
+        if (currAttendance < maxAttendance) {
+            currAttendance++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+}
