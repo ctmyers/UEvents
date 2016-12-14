8311756ce37b0b48814e01ecbd9271e811b59502
andysoto <andrea.soto2011@gmail.com>
2016-12-14 01:15:45 -0500

    comments for CreateEventActivity

--- app/src/main/java/com/uevents/app/uevents/CreateEventActivity.java
+++ app/src/main/java/com/uevents/app/uevents/CreateEventActivity.java
@@ -26,13 +26,16 @@ import java.util.Locale;
 
 /**
  * Created by Andrea on 12/5/2016.
+ * CreateEventActivity is started when the floating action button
+ * is clicked. A create form appears which the user can fill out
+ * and click save to finish the creation process or cancel at any time.
+ * Source for data and time picker:
  * https://github.com/CiTuX/datetimepicker
  */
 
 public class CreateEventActivity extends AppCompatActivity implements
         DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
     private static final String TIME_PATTERN = "HH:mm";
-
     private Dialog dateTimePicker;
     private TextView dateTextView;
     private TextView timeTextView;
@@ -46,23 +49,21 @@ public class CreateEventActivity extends AppCompatActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_event_form);
 
+        // Set up calendar, date and time format variables
         calendar = Calendar.getInstance();
         dateFormat = DateFormat.getDateInstance(DateFormat.LONG, Locale.getDefault());
         timeFormat = new SimpleDateFormat(TIME_PATTERN, Locale.getDefault());
 
+        // Set up the date and time text views
         dateTextView = (TextView) findViewById(R.id.dateTextView);
         timeTextView = (TextView) findViewById(R.id.timeTextView);
         dateTextView.setText(dateTextView.getText());
         timeTextView.setText(timeTextView.getText());
 
-        /*dateTimePicker = new Dialog(this);
-        View dialogLayout = getLayoutInflater().inflate(R.layout.date_time_picker_dialog,
-                (ViewGroup)findViewById(R.id.dateTime_dialog));
-        dateTimePicker.setContentView(dialogLayout);*/
-
+        // Event Title edit text
         eventNameEditText = (EditText) findViewById(R.id.eventTitle);
 
-
+        // Set up category spinner
         Spinner spinner = (Spinner) findViewById(R.id.categorySpinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -75,11 +76,7 @@ public class CreateEventActivity extends AppCompatActivity implements
 
     }
 
-    private void update() {
-        dateTextView.setText(dateTextView.getText() + dateFormat.format(calendar.getTime()));
-        timeTextView.setText(timeTextView.getText() + timeFormat.format(calendar.getTime()));
-    }
-
+    // Create dialogs for date and time picker when date or time button clicked
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.datePickerButton:
@@ -91,26 +88,33 @@ public class CreateEventActivity extends AppCompatActivity implements
         }
     }
 
+    // Sets the date text view once date selection is made
     @Override
     public void onDateSet(DatePickerDialog dialog, int year, int monthOfYear, int dayOfMonth) {
         calendar.set(year, monthOfYear, dayOfMonth);
-        //update();
         dateTextView.setText(dateTextView.getText() + dateFormat.format(calendar.getTime()));
     }
 
+    // Sets time text view once time selection is made
     @Override
     public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute) {
         calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
         calendar.set(Calendar.MINUTE, minute);
-        //update();
         timeTextView.setText(timeTextView.getText() + timeFormat.format(calendar.getTime()));
     }
+
+    // Activity finishes when event creation is cancelled by clicking the cancel button
     public void onCancelClick(View v){
         Intent returnIntent = new Intent();
         setResult(Activity.RESULT_CANCELED,returnIntent);
         finish();
     }
 
+    /* When save button clicked, checks that the entire form is filled out
+     * before completing the creation process and notifies user if the form
+     * is incomplete. Otherwise, event information is stored and sent back
+     * to the MainActivity to create an event and update the map and list views.
+     */
     public void onSaveButtonClick(View v) {
         EditText eventDes = (EditText)findViewById(R.id.eventDescription);
         EditText maxAttend = (EditText)findViewById(R.id.maxAttendees);
@@ -143,12 +147,4 @@ public class CreateEventActivity extends AppCompatActivity implements
 
     }
 
-    /*eventNameEditText.setOnFocusChangeListener(new View.OnFocusChangeListener(){
-        public void onFocusChange(View v, boolean hasFocus){
-            if(hasFocus)
-                eventNameEditText.setHint("");
-            else
-                eventNameEditText.setHint("Your hint");
-        }
-    });*/
 }
